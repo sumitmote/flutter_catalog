38f6d283c2cc8ec605597c6ff8dce69ffd9e8e2f
sumitmote <sumitmote19@gmail.com>
2022-07-24 20:56:14 +0530

    Login Page

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -21,16 +21,35 @@ class LoginPage extends StatelessWidget {
             SizedBox(
               height: 20.0,
             ),
-            TextFormField(
-              decoration: InputDecoration(
-                hintText: "Please enter username",
-                labelText: "Username",
-              ),
-            ),
-            TextFormField(
-              decoration: InputDecoration(
-                hintText: "Please enter password",
-                labelText: "Password",
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Please enter username",
+                      labelText: "Username",
+                    ),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: "Please enter password",
+                      labelText: "Password",
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  ElevatedButton(
+                    child: Text("Login"),
+                    style: TextButton.styleFrom(),
+                    onPressed: () {
+                      print("HI DEMO TEST");
+                    },
+                  )
+                ],
               ),
             )
           ],
